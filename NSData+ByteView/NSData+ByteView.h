fdf8370d51c5223cb4fdc50afda84d865dda5bd3
Michael Radtke <mradtke@abigale.de>
2015-11-15 13:50:14 +0100

    Initial Commit

---
+++ NSData+ByteView/NSData+ByteView.h
@@ -0,0 +1,19 @@
+//
+//  NSData+ByteView.h
+//  NSData+ByteView
+//
+//  Created by Michael Radtke on 15.11.15.
+//  Copyright © 2015 abigale solutions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSData+ByteView.
+FOUNDATION_EXPORT double NSData_ByteViewVersionNumber;
+
+//! Project version string for NSData+ByteView.
+FOUNDATION_EXPORT const unsigned char NSData_ByteViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSData_ByteView/PublicHeader.h>
+
+
